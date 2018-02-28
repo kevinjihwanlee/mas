c4e46025cd9cdf4878dd21a54bd0a5b080da21e0
Itay <32287673+itaygolan@users.noreply.github.com>
2018-02-27 22:07:50 -0600

    Add files via upload

---
+++ mas_find_classes/reg_exp.js
@@ -0,0 +1,35 @@
+
+// ********* PARSE REQUIREMENTS ***********
+
+const requirements = "Enrollment Requirements: Prerequisite: Students must completed EECS 172-0 or EECS 103-0 or EECS 123-2 to register for this course."
+
+
+function string_parser(course) {
+  if (!course) {
+    return [];
+  }
+  const words_array = course.match(/\b(\w|')+\b/gim);
+  const class_and_number = [];
+
+  words_array.forEach((word, i) => {
+    if (word ==  `EECS`) {
+      class_and_number.push(word);
+      class_and_number.push(words_array[i+1]);
+      class_and_number.push(words_array[i+2])
+    }
+  });
+
+  const classes = [];
+  let i = 0;
+  class_and_number.forEach(() => {
+    if (i + 2 <= class_and_number.length) {
+      const s = `${class_and_number[i]} ${class_and_number[i+1]}-${class_and_number[i+2]}`;
+      classes.push(s);
+      i += 3;
+    }
+  });
+
+  return classes;
+}
+
+module.exports = string_parser;
